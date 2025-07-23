1fc0a819a224a94b22e443ed54050db62248eb0d
anjali <anjalisharma24@navgurukul.org>
2025-07-23 11:18:17 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import "slick-carousel/slick/slick.css"; // Isko add karein
 import "slick-carousel/slick/slick-theme.css"; // Isko bhi add karein
 import './App.css';
-import About from './Components/About';
-import Skills from './Components/Skills';
-import Experience from './Components/Experience';
-import Education from './Components/Education';
-import Contact from './Components/Contact';
-import Footer from './Components/Footer';
-import Projects from './Components/Projects';
-import Navbar from './Components/Navbar';
-import Certificates from './Components/Certificates';
+import About from './components/About';
+import Skills from './components/Skills';
+import Experience from './components/Experience';
+import Education from './components/Education';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Projects from './components/Projects';
+import Navbar from './components/Navbar';
+import Certificates from './components/Certificates';
 
 function App() {
   return (
